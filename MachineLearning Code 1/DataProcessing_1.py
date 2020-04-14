a45492aa7cff0250aceb2969a7fcd02b0cd859b3
mukulietlucknow <mukul.ietlucknow@gmail.com>
2020-04-14 22:34:37 +0530

    some changes

--- MachineLearning Code 1/DataProcessing_1.py
+++ MachineLearning Code 1/DataProcessing_1.py
@@ -9,6 +9,11 @@ dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 Y = dataset.iloc[:, 3].values
 
+#handling missing data
+from sklearn.impute import SimpleImputer
+imputer  = SimpleImputer(missing_values = np.nan , strategy='mean')
+imputer.fit(X[: , 1:3])
+X[: , 1:3] = imputer.transform(X[: , 1:3])
 
 Y = LabelEncoder().fit_transform(Y) # Encoding Y data
 # Splitting the dataset into the Training set and Test set
